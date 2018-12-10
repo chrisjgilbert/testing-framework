d27249e9060ee02a8371f32e3d969ab95338dd65
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-10 16:08:18 +0000

    testjs namespacing implemented and color printed on result

--- squareSpec.js
+++ squareSpec.js
@@ -1,37 +1,39 @@
-function test(description, assertion) {
-  printDescription(description);
-  assertion();
-};
+var testjs = {
 
-function assertEquals(result, expected) {
-  return (result === expected ? passTest() : failTest(result, expected));
-};
+  test: function(description, assertion) {
+    this.printDescription(description);
+    assertion();
+  },
 
-function printResult(result, color) {
-  document.getElementById("result").innerHTML = "Test "+ result +"!";
-  document.getElementById("result").style.color = color
+  assertEquals: function(result, expected) {
+    return (result === expected ? this.passTest() : this.failTest(result, expected));
+  },
+
+  printResult: function(result, color) {
+    document.getElementById("result").innerHTML = "Test "+ result +"!";
+    document.getElementById("result").style.color = color
+  },
+
+  printDescription: function(description) {
+    document.getElementById("description").innerHTML = description;
+  },
+
+  passTest: function() {
+    this.printResult('passed', 'green');
+  },
+
+  failTest: function(result, expected) {
+    this.printResult('fail', 'red');
+    throw new Error('Expect result: ' + expected + ' Actual result: ' + result)
+  }
 }
 
-function printDescription(description) {
-  document.getElementById("description").innerHTML = description;
-}
-
-function passTest() {
-  printResult('passed', 'green');
-}
-
-function failTest(result, expected) {
-  printResult('fail', 'red');
-  throw new Error('Expect result: ' + expected + ' Actual result: ' + result)
-}
-
-
-test('a passing test', function() {
+testjs.test('a passing test', function() {
     var square = new Square(10);
-    assertEquals(square.area(), 100);
+    testjs.assertEquals(square.area(), 100);
 });
 
-test('a failing test', function() {
+testjs.test('a failing test', function() {
     var square = new Square(10);
-    assertEquals(square.area(), 3);
+    testjs.assertEquals(square.area(), 1);
 });
